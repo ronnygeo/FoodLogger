82c71fd009fa0fff2c5bcc2db8b56cb86ae5046e
ronnygeo <ronnygeomatt@gmail.com>
2016-05-20 00:29:57 -0500

    Initial commit.

--- FoodLogger/ViewController.swift
+++ FoodLogger/ViewController.swift
@@ -10,16 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Properties/Outlets
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    /*override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
+    }*/
 
+    // MARK: Actions
+    @IBAction func resetButton(sender: UIButton) {
+        nameLabel.text = "Default Text"
+    }
+    
 
 }
 
